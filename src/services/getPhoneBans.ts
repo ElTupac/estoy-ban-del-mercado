f6f00a86fa6336b9e790cc1017d6ebff4a066358
NehuenCovelo <nehuencovelo@gmail.com>
2024-10-08 11:10:03 -0300

    fix: correctly returning warning and ban object

--- src/services/getPhoneBans.ts
+++ src/services/getPhoneBans.ts
@@ -5,8 +5,15 @@ import { Ban } from "../entities";
 export const getPhoneBans: (
   phone_id: UUID,
   banRepository: Repository<Ban>
-) => Promise<Ban[]> = (phone_id, banRepository) =>
-  banRepository
-    .createQueryBuilder("")
-    .where("phone_id = :phone_id", { phone_id })
-    .execute() as Promise<Ban[]>;
+) => Promise<Ban[]> = async (phone_id, banRepository) =>
+  (
+    await (banRepository
+      .createQueryBuilder("ban")
+      .where("ban.phone_id = :phone_id", { phone_id })
+      .execute() as Promise<Ban[]>)
+  ).map((ban) =>
+    Object.entries(ban).reduce(
+      (accum, [key, value]) => ({ ...accum, [key.replace("ban_", "")]: value }),
+      {}
+    )
+  ) as Ban[];
--- src/services/getPhoneWarnings.ts
+++ src/services/getPhoneWarnings.ts
@@ -5,8 +5,18 @@ import { Warning } from "../entities";
 export const getPhoneWarnings: (
   phone_id: UUID,
   warningRepository: Repository<Warning>
-) => Promise<Warning[]> = (phone_id, warningRepository) =>
-  warningRepository
-    .createQueryBuilder("")
-    .where("phone_id = :phone_id", { phone_id })
-    .execute() as Promise<Warning[]>;
+) => Promise<Warning[]> = async (phone_id, warningRepository) =>
+  (
+    await (warningRepository
+      .createQueryBuilder("warning")
+      .where("warning.phone_id = :phone_id", { phone_id })
+      .execute() as Promise<Warning[]>)
+  ).map((warning) =>
+    Object.entries(warning).reduce(
+      (accum, [key, value]) => ({
+        ...accum,
+        [key.replace("warning_", "")]: value,
+      }),
+      {}
+    )
+  ) as Warning[];
